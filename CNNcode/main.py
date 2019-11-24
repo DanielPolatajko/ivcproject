f245a4a476d98b36ad374ea6d0fe3a9603b823d8
Daniel Polatajko <danielpolatajko@college.harvard.edu>
2019-11-24 16:04:34 +0000

    a

--- CNNcode/main.py
+++ CNNcode/main.py
@@ -36,10 +36,10 @@ def main():
 
     tvt_split = (0.5,0.7)
 
-    X_train, X_val, X_test, y_train, y_val, y_test = generate_dataset_static(data_path,mask_path,tvt_split, N)
+    #X_train, X_val, X_test, y_train, y_val, y_test = generate_dataset_static(data_path,mask_path,tvt_split, N)
     X_train_t, X_val_t, X_test_t, y_train_t, y_val_t, y_test_t = generate_dataset_temporal(data_path, mask_path,tvt_split, N)
 
-    X_train = np.array(X_train).swapaxes(1,3).swapaxes(2,3)
+    """X_train = np.array(X_train).swapaxes(1,3).swapaxes(2,3)
     X_val = np.array(X_val).swapaxes(1,3).swapaxes(2,3)
     X_test = np.array(X_test).swapaxes(1,3).swapaxes(2,3)
     print(X_train.shape)
@@ -50,7 +50,7 @@ def main():
     y_test = np.array(y_test)
     print(y_train.shape)
     print(y_val.shape)
-    print(y_test.shape)
+    print(y_test.shape)"""
 
     X_train_t = np.array(X_train_t).swapaxes(-1,-3).swapaxes(-2,-1)
     X_val_t = np.array(X_val_t).swapaxes(-1,-3).swapaxes(-2,-1)
@@ -66,26 +66,28 @@ def main():
     print(y_test_t.shape)
 
     batch_size=25
-    train_data = data_providers.DataProvider(X_train,y_train,batch_size,shuffle_order=True)
+    """train_data = data_providers.DataProvider(X_train,y_train,batch_size,shuffle_order=True)
     val_data = data_providers.DataProvider(X_val,y_val,batch_size,shuffle_order=True)
-    test_data = data_providers.DataProvider(X_test,y_test,batch_size,shuffle_order=True)
+    test_data = data_providers.DataProvider(X_test,y_test,batch_size,shuffle_order=True)"""
 
     train_data_t = data_providers.DataProvider(X_train_t,y_train_t,batch_size,shuffle_order=True)
     val_data_t = data_providers.DataProvider(X_val_t,y_val_t,batch_size,shuffle_order=True)
     test_data_t = data_providers.DataProvider(X_test_t,y_test_t,batch_size,shuffle_order=True)
 
-    inputs_shape = X_train[:batch_size].shape
-    inputs_shape
+    #inputs_shape = X_train[:batch_size].shape
+    #inputs_shape
 
     inputs_shape_t = X_train_t[:batch_size].shape
     inputs_shape_t
 
-    static_net_shallow=ShallowNetwork(input_shape=inputs_shape)
-    static_net_deeper=ShallowNetwork(input_shape=inputs_shape)
-    temporal_net_shallow = ShallowNetwork(input_shape=inputs_shape_t)
-    temporal_net_deeper = ShallowNetwork(input_shape=inputs_shape_t)
+    print("Time to make networks!")
 
-    experiment_name= "static_run_shallow"
+    #static_net_shallow=ShallowNetwork(input_shape=inputs_shape)
+    #static_net_deeper=DeeperNetwork(input_shape=inputs_shape)
+    temporal_net_shallow = ShallowNetwork(input_shape=inputs_shape_t)
+    #temporal_net_deeper = DeeperNetwork(input_shape=inputs_shape_t)
+
+    """experiment_name= "static_run_shallow"
     num_epochs = 30
     use_gpu=False
     continue_from_epoch=-1
@@ -111,7 +113,7 @@ def main():
                                         continue_from_epoch=continue_from_epoch,
                                         train_data=train_data, val_data=val_data,
                                         test_data=test_data)  # build an experiment object
-    experiment_metrics, test_metrics = experiment_1.run_experiment()  # run experiment and return experiment metrics
+    experiment_metrics, test_metrics = experiment_1.run_experiment()  # run experiment and return experiment metrics"""
 
     experiment_name= "temporal_run_shallow"
     num_epochs = 30
@@ -127,7 +129,7 @@ def main():
                                         test_data=test_data_t)  # build an experiment object
     experiment_metrics, test_metrics = experiment_1.run_experiment()  # run experiment and return experiment metrics
 
-    experiment_name= "temporal_run_deeper"
+    """experiment_name= "temporal_run_deeper"
     num_epochs = 30
     use_gpu=True
     continue_from_epoch=-1
@@ -139,7 +141,7 @@ def main():
                                         continue_from_epoch=continue_from_epoch,
                                         train_data=train_data_t, val_data=val_data_t,
                                         test_data=test_data_t)  # build an experiment object
-    experiment_metrics, test_metrics = experiment_1.run_experiment()  # run experiment and return experiment metrics
+    experiment_metrics, test_metrics = experiment_1.run_experiment()  # run experiment and return experiment metrics"""
 
 if __name__ == '__main__':
     main()
