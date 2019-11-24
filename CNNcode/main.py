6aff3f665674e4338786dd803ef805acb1e36c32
Daniel Polatajko <danielpolatajko@college.harvard.edu>
2019-11-24 17:25:28 +0000

    a

--- CNNcode/main.py
+++ CNNcode/main.py
@@ -26,7 +26,7 @@ import time
 
 def main():
 
-    N = 1
+    N = 4
 
     # define paths to image and mask files
     cwd = Path(os.getcwd())
@@ -39,6 +39,7 @@ def main():
     X_train, X_val, X_test, y_train, y_val, y_test = generate_dataset_static(data_path,mask_path,tvt_split, N)
     #X_train_t, X_val_t, X_test_t, y_train_t, y_val_t, y_test_t = generate_dataset_temporal(data_path, mask_path,tvt_split, N)
 
+    """
     X_train = np.array(X_train).swapaxes(1,3).swapaxes(2,3)
     X_val = np.array(X_val).swapaxes(1,3).swapaxes(2,3)
     X_test = np.array(X_test).swapaxes(1,3).swapaxes(2,3)
@@ -51,8 +52,9 @@ def main():
     print(y_train.shape)
     print(y_val.shape)
     print(y_test.shape)
-
     """
+
+
     X_train_t = np.array(X_train_t).swapaxes(-1,-3).swapaxes(-2,-1)
     X_val_t = np.array(X_val_t).swapaxes(-1,-3).swapaxes(-2,-1)
     X_test_t = np.array(X_test_t).swapaxes(-1,-3).swapaxes(-2,-1)
@@ -64,7 +66,9 @@ def main():
     y_test_t = np.array(y_test_t)
     print(y_train_t.shape)
     print(y_val_t.shape)
-    print(y_test_t.shape)  """
+    print(y_test_t.shape)
+
+    """
 
     batch_size=25
     train_data = data_providers.DataProvider(X_train,y_train,batch_size,shuffle_order=True)
@@ -74,19 +78,20 @@ def main():
     """
     train_data_t = data_providers.DataProvider(X_train_t,y_train_t,batch_size,shuffle_order=True)
     val_data_t = data_providers.DataProvider(X_val_t,y_val_t,batch_size,shuffle_order=True)
-    test_data_t = data_providers.DataProvider(X_test_t,y_test_t,batch_size,shuffle_order=True)"""
+    test_data_t = data_providers.DataProvider(X_test_t,y_test_t,batch_size,shuffle_order=True)
 
-    inputs_shape = X_train[:batch_size].shape
-    inputs_shape
 
-    #inputs_shape_t = X_train_t[:batch_size].shape
-    #inputs_shape_t
+    #inputs_shape = X_train[:batch_size].shape
+    #inputs_shape
+
+    inputs_shape_t = X_train_t[:batch_size].shape
+    inputs_shape_t
 
     print("Time to make networks!")
 
-    static_net_shallow=ShallowNetwork(input_shape=inputs_shape)
+    #static_net_shallow=ShallowNetwork(input_shape=inputs_shape)
     #static_net_deeper=DeeperNetwork(input_shape=inputs_shape)
-    #temporal_net_shallow = ShallowNetwork(input_shape=inputs_shape_t)
+    temporal_net_shallow = ShallowNetwork(input_shape=inputs_shape_t)
     #temporal_net_deeper = DeeperNetwork(input_shape=inputs_shape_t)
 
     """experiment_name= "static_run_shallow"
