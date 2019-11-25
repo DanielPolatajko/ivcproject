1ff10b922ecccb3e5172195f9bc5a51eab4169fb
Daniel Polatajko <danielpolatajko@college.harvard.edu>
2019-11-25 00:17:25 +0000

    a

--- CNNcode/main.py
+++ CNNcode/main.py
@@ -8,7 +8,7 @@ from torchvision import transforms
 
 import data_provider as data_providers
 from experiment_builder import ExperimentBuilder
-from models import ShallowNetwork, DeeperNetwork
+from models import ShallowNetwork, DeeperNetwork, DeepestNetwork
 import storage_utils as storage_utils
 from utils import generate_dataset_static, generate_dataset_temporal, jaccard_loss
 
@@ -36,10 +36,10 @@ def main():
 
     tvt_split = (0.5,0.7)
 
-    #X_train, X_val, X_test, y_train, y_val, y_test = generate_dataset_static(data_path,mask_path,tvt_split, N)
+    X_train, X_val, X_test, y_train, y_val, y_test = generate_dataset_static(data_path,mask_path,tvt_split, N)
     X_train_t, X_val_t, X_test_t, y_train_t, y_val_t, y_test_t = generate_dataset_temporal(data_path, mask_path,tvt_split, N)
 
-    """
+
     X_train = np.array(X_train).swapaxes(1,3).swapaxes(2,3)
     X_val = np.array(X_val).swapaxes(1,3).swapaxes(2,3)
     X_test = np.array(X_test).swapaxes(1,3).swapaxes(2,3)
@@ -52,7 +52,7 @@ def main():
     print(y_train.shape)
     print(y_val.shape)
     print(y_test.shape)
-    """
+
 
 
     X_train_t = np.array(X_train_t).swapaxes(-1,-3).swapaxes(-2,-1)
@@ -68,14 +68,14 @@ def main():
     print(y_val_t.shape)
     print(y_test_t.shape)
 
-    """
+
 
     batch_size=25
     train_data = data_providers.DataProvider(X_train,y_train,batch_size,shuffle_order=True)
     val_data = data_providers.DataProvider(X_val,y_val,batch_size,shuffle_order=True)
     test_data = data_providers.DataProvider(X_test,y_test,batch_size,shuffle_order=True)
 
-    """
+
 
     batch_size = 25
     train_data_t = data_providers.DataProvider(X_train_t,y_train_t,batch_size,shuffle_order=True)
@@ -83,8 +83,8 @@ def main():
     test_data_t = data_providers.DataProvider(X_test_t,y_test_t,batch_size,shuffle_order=True)
 
 
-    #inputs_shape = X_train[:batch_size].shape
-    #inputs_shape
+    inputs_shape = X_train[:batch_size].shape
+    inputs_shape
 
     inputs_shape_t = X_train_t[:batch_size].shape
     inputs_shape_t
@@ -94,7 +94,9 @@ def main():
     #static_net_shallow=ShallowNetwork(input_shape=inputs_shape)
     #static_net_deeper=DeeperNetwork(input_shape=inputs_shape)
     #temporal_net_shallow = ShallowNetwork(input_shape=inputs_shape_t)
-    temporal_net_deeper = DeeperNetwork(input_shape=inputs_shape_t)
+    #temporal_net_deeper = DeeperNetwork(input_shape=inputs_shape_t)
+    static_net_deepest = DeepestNetwork(input_shape=inputs_shape)
+    temporal_net_deepest = DeepestNetwork(input_shape=inputs_shape_t)
 
     """experiment_name= "static_run_shallow"
     num_epochs = 30
@@ -138,7 +140,7 @@ def main():
                                         test_data=test_data_t)  # build an experiment object
     experiment_metrics, test_metrics = experiment_3.run_experiment()  # run experiment and return experiment metrics"""
 
-    experiment_name= "temporal_run_deeper"
+    """experiment_name= "temporal_run_deeper"
     num_epochs = 30
     use_gpu=True
     continue_from_epoch=-1
@@ -151,6 +153,34 @@ def main():
                                         train_data=train_data_t, val_data=val_data_t,
                                         test_data=test_data_t)  # build an experiment object
     experiment_metrics, test_metrics = experiment_4.run_experiment()  # run experiment and return experiment metrics"""
+
+    experiment_name= "static_run_deepest"
+    num_epochs = 30
+    use_gpu=True
+    continue_from_epoch=-1
+
+    experiment_5 = ExperimentBuilder(network_model=static_net_deepest,
+                                        experiment_name=experiment_name,
+                                        num_epochs=num_epochs,
+                                        use_gpu=use_gpu,
+                                        continue_from_epoch=continue_from_epoch,
+                                        train_data=train_data, val_data=val_data,
+                                        test_data=test_data)  # build an experiment object
+    experiment_metrics, test_metrics = experiment_5.run_experiment()  # run experiment and return experiment metrics
+
+    experiment_name= "temporal_run_deepest"
+    num_epochs = 50
+    use_gpu=True
+    continue_from_epoch=-1
+
+    experiment_6 = ExperimentBuilder(network_model=temporal_net_deepest,
+                                        experiment_name=experiment_name,
+                                        num_epochs=num_epochs,
+                                        use_gpu=use_gpu,
+                                        continue_from_epoch=continue_from_epoch,
+                                        train_data=train_data_t, val_data=val_data_t,
+                                        test_data=test_data_t)  # build an experiment object
+    experiment_metrics, test_metrics = experiment_6.run_experiment()  # run experiment and return experiment metrics
 
 if __name__ == '__main__':
     main()
